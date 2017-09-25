81208c2bc18e6605ad230eb62cc789624f624c19
Emmanuel Taborda Carmona <ETabordaC@DANE.GOV.CO>
2017-09-25 13:40:18 -0500

    Agregando módulo joomDoc y creando archivos prueba

---
+++ administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-09-25 16:58:06 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-09-25T16:58:06+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2017-09-25T16:58:14+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2017-09-25T16:58:25+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2017-09-25T16:58:30+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2017-09-25T16:58:37+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
